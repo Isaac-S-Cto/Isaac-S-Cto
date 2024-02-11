9d46babf7c32a9886947f139db23d411df0d6b15
isaac.s.cto <isaac.dev89@gmail.com>
2024-02-11 18:23:46 -0300

    update-03

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,77 @@
+import { ChangeEvent, useState } from 'react'
 import logo from './assets/Logo-slw.svg' /* importação da logo */
-import { NewNoteCard, Notecard } from './assets/componentes/note-cards'
+import { NewNoteCard, Notecard } from './assets/componentes/note-cards' 
+/* NewNotecards/Notecards importação dos componentes para fazer comunicação entre elementos*/
 
-const note = {
-  date: new Date(),
-  content: 'hello word'
+interface Note{
+/* formato do array*/
+  id: string
+  date: Date
+  content: string
 }
 
-export function App() {
+
+
+ export function App() {
+
+  const [search, setSearch] = useState('') 
+  const [notes, setNotes] = useState<Note[]>(() => {  /*iniciando o estado com uma arrow function > return */
   
 
-  return (
-   /* configurando o conteiner*/
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+  /* criando const notesOn/storage para receber os 
+  itens do local storage(api-browser. caso haja um item chamado notes: retorne : JASON) no notesOnStorage  */
+  const notesOnStorage = localStorage.getItem('notes')
+    
+    if (notesOnStorage){
+      return JSON.parse(notesOnStorage) /* .parce > caminho contrario do stringfy */ 
+    }
+    return[]
+
+}) /* < informando como sera o formato do meu array > interface note */
+
+
+
+
+function handleSearch(event: ChangeEvent<HTMLInputElement>){ /*função recebe o evento:onchange *//*HTMLImputElement > informa o elemento que esta recendo o evento*/
+  const querry = event.target.value /* criando variavel para receber o valor da busca  */
+  setSearch(querry) /* e enrão salvamos o valor dentro do estado da busca */
+}
+
+const filterNotes = search != '' /* caso meu search seja diferente de uma string vazia*/
+? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())) /* buscar as notas  onde o conteundo inclui a palavra buscada */
+:notes 
+ /*caso esteja vazio eu retorno somente o display de notas */
+ 
+  function onNoteCreated(content: string) { 
+  
+    const NewNote = {
+      
+
+    id: crypto.randomUUID(), /* cryptoRamdom gera uma id aleatório do tipo string* > NewNote > id:notes/ date > new Date(),
+    content > conteudo virá do parametro content : string */
+    date: new Date(),
+    content,
+    
+ }
+ /*Salvando as notas no navegador > Local.Storage */
+ const notesArray = [NewNote, ...notes] /*separando o array em uma variavel*/
+   setNotes(notesArray) /* passando os parametris para a variavel da function app; setNotes > Notes*/
+   localStorage.setItem('notes', JSON.stringify(notesArray))
+   /* local.stotage > api do browser para guardar infoemações.setItem > enviar informações.
+    notes > nomear . Json.stringify: local.store , nao aceita array entao com este comando convertemos em texto > Json.string */
+  }
+  function onNoteDeleted(id: String){
+    const notesArray = notes.filter(note =>{
+return note.id != id
+    })
+
+ setNotes(notesArray)
+ localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
+return (
+   /* configurando o conteiner/ tela pricipal do app*/
+    <div className="mx-auto max-w-6xl my-12 space-y-6  px-5">
     {/* chamando a imagem dentro da aplição <img src> / 
     ultiliza-se aspas para colocar uma variavel JS dentro do html */}
     <img src={logo} alt='logo-slw'/> 
@@ -21,23 +81,31 @@ export function App() {
   type="text" placeholder='Busque em suas notas...' 
   /* w-full > tela inteira/ text-3xl > tamanho font/ trancking > diminui os espaçoes entre as letras/outline none> remove o focus */
   className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none"
+  onChange={handleSearch} /* toda vez que você mudar o seu valor vou chamar a função handleSearch*/
 />
       </form>
 
 
-      <div className="h-px bg-slate-700"></div>
+<div className="h-px bg-slate-700"/>
 
-       <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
-
-    {/*elemento criado na pasta componets /* Grids principal*/}
-     <NewNoteCard/>
-
+<div className="grid grid-col-1 md:grid-col-2 lg:grid-cols-3 auto-rows-[250px] gap-6">
+         {/*elemento criado na pasta componets /* Grids principal*/}
+    {/* passando a funçao onoNote created para do meu componet criado*/}
+      {/* é preciso infomar lá no componetente que ele recebera valor void}
 
            {/*elemento criado na pasta componets no arquivo Note-cards
            implementando uma função e chamando no App.tsx para não deixar o codigo tão grande*/}
-      <Notecard note={note}/>
-    
-          
+      <NewNoteCard onNoteCreated={onNoteCreated}/>
+           {/*o componente newnotecard recebe a função onNoteCreated */}
+           
+           {filterNotes.map((note) => {   
+            
+            /* map percorre a array(as notas) */
+            return ( <Notecard key={note.id} note={note} onNoteDeleted = {onNoteDeleted} /> /* key > propriedade do react que atribui um valor unico 
+            para cada posição do array(notas) Obrigatório
+            e irá retornar um notecard passando pra ele as informações da nota */
+          );
+        })}
           
           
       </div>    
--- src/assets/componentes/note-cards.tsx
+++ src/assets/componentes/note-cards.tsx
@@ -1,56 +1,138 @@
-/* importando radix dialog - instalado atraves das informações do site radix.ui , executando no terminal o comando npm  */
+/* importando radix -> lib de componentes >dialog - instalado atraves das informações do site radix.ui , executando no terminal o comando npm  */
 /* criando variavel Dialog para receber a importação  */
 import * as Dialog from '@radix-ui/react-dialog'
 import {formatDistanceToNow } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
+/* lucile > lib -> importação de icons */
 import {X} from 'lucide-react'
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, FormEvent, useState } from 'react'
+
+/* lib para msg de alerta na tela */
 import { toast } from 'sonner'
-
-
-
-
-
 
 
 interface NoteCardProps{ /* criando uma propriedade global*/
   
 /*crio 1 propriedade para receber 2 props/ com chaves indico que é um objeto e recebo as duas propriedades */
     note: { 
+      id: string
     date: Date
     content: string
      }
+    onNoteDeleted: (id:string) => void
+}
+interface NewNoteCardProps{
+  onNoteCreated: (content: string) => void /* informando que newNotes recebera
+   um valor vazia da nossa funçao - onNote created e seu conteudo 
+   sera chamado na função handleSaveNotes */
 }
 
+
+let speechRecognition: SpeechRecognition | null = null
+
+
+export function NewNoteCard({onNoteCreated}: NewNoteCardProps) {
 /* função Card-Pricipal ADD NOTA  */    
-  export function NewNoteCard(){
+
+
 
   const [ShouldShowOnboarding, setShouldShowOnboarding] = useState(true) /* criando variavel*/
   const [content, setContent] = useState('')
+  const [isRecording, setIsRecording] = useState(false);
   /* usestate > criando um estado > variavel booleana > o usestate cria um array > que na linha recebe as 2 variaveis criadas que ocupam a 1 e 2 pos do array*/
   
   function HandleStartEdition(){
-    /* criando a função que receberá o valor de falso quando o suario clicar no button para inserir informação <input> */
-    setShouldShowOnboarding(false)
+    /* criando a função que receberá o valor de falso quando o usuario clicar no button para inserir informação <input> */
+    setShouldShowOnboarding(false);
   }
 
   /* função do evento criado dentro do modal TEXT AREA/ passando variaveis do tipo boolean  */
-  function handleContenChange(event:ChangeEvent<HTMLTextAreaElement>){ /* value(HTML-TEXT-AREA-ELEMENT) > é necessario expecificar pois o evento onchange nao é especifico da text area, pode vir de varios elemento do html */ 
+  function handleContentChange(event:ChangeEvent<HTMLTextAreaElement>){ /* value(HTML-TEXT-AREA-ELEMENT) > é necessario expecificar pois o evento onchange nao é especifico da text area, pode vir de varios elemento do html */ 
 
   setContent(event.target.value) /* passando valores do evento submit(Savar notas) atraves das variaveis/ 
 content <= recera o valor de submit */
   if (event.target.value === ''){
-setShouldShowOnboarding(true)
+setShouldShowOnboarding(false)
+
+
   }
 }
 
-function HandleSaveNote(event:ChangeEvent<HTMLFormElement>){
-  event.preventDefault()
-toast.success('nota criada com sucesso')
-}
-  
-  return (
 
+/* função de criação de notas*/
+function HandleSaveNote(event:FormEvent){
+  event.preventDefault()
+
+  if ( content === '') {
+    return 
+  }
+
+ 
+
+
+  onNoteCreated(content)
+  setContent('') /*resetando o conteudo da minha text area */
+setShouldShowOnboarding(true) /* mostrar a tela de adicionar nota por audio ou texto após salvar uma nota */
+toast.success('nota criada com sucesso')
+
+
+}
+
+  
+function handleStartRecording(){
+
+
+const isSpeechRecognationAPIAvailable = 'isSpeechRecognation' in window
+|| 'webkitSpeechRecognition' in window
+
+if(!isSpeechRecognationAPIAvailable ){
+alert('browser Not suported API recordin')
+return
+}
+setIsRecording(true)
+setShouldShowOnboarding(false)
+
+const SpeechReconitionAPI = window.SpeechRecognition || window.webkitSpeechRecognition
+ 
+speechRecognition = new SpeechReconitionAPI()
+
+
+speechRecognition.lang = 'pt-BR' /* idioma do gravador*/
+speechRecognition.continuous = true /* gravá continuamente ate voce interromper a gravação*/
+speechRecognition.maxAlternatives = 1 /* só trara uma opção de palavra */
+speechRecognition.interimResults = true /*trará resultados em tempo real, e nao apenas quando parar de falar */
+
+speechRecognition.onresult = (event) => {
+  const transcription = Array.from (event.results).reduce((text, result) => {
+    return text.concat(result[0].transcript)
+
+  },'')
+  setContent(transcription)
+}
+speechRecognition.onerror = (event) => {
+  console.error(event)
+
+}
+speechRecognition.start()
+}
+
+function handleStopRecording(){
+  
+  setIsRecording(false)
+  
+  if (speechRecognition != null){
+speechRecognition.stop()
+  }
+  
+  }
+
+  
+
+
+
+
+
+  return (
 
     <Dialog.Root>
 
@@ -69,17 +151,20 @@ toast.success('nota criada com sucesso')
            
            <Dialog.Overlay className="inset-0 fixed bg-black/50 "/> {/* overlay > adiciona o efeito do modal, de estar por cima da aplicação/ inset-0 > para ocupar a tela toda/ fixed para nao ter scroll*/}
             
-            <Dialog.Content className="fixed left-1/2 top-1/2 overflow-hidden -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none "> 
+            <Dialog.Content className="fixed inset-0 md:inset-auto md:left-1/2 md:top-1/2 overflow-hidden md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none "> 
             {/* -translate >  força o elemento a fixar-se no centro da tela - pois por padrao ele se ajuta ao centro baseando pela ponta superior esquerda do objeto */}
               {/* h-[60vh] vh define medida em relação a tela, quando usamos % ele usa como base o tamanho do elemento pai */}
              
+            
+            
 
-             <Dialog.Close className=" absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 ">
-            <X className="size-5 hover:text-slate-100"/>
-             </Dialog.Close>
+             <Dialog.Close 
+             className=" absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 ">
+            < X className="size-5 hover:text-slate-100"/>
+               </Dialog.Close>
 
 
-             <form onSubmit={HandleSaveNote} className=" flex-1 flex flex-col">
+             <form className=" flex-1 flex flex-col">
              {/* on submit > criando um evendo para salvar as notas */}
 
               <div className="flex flex-1 flex-col gap-3 p-5 ">
@@ -97,12 +182,18 @@ toast.success('nota criada com sucesso')
                  /* se a variaver for (true'?') */
                  
                  <p className="text-sm leading-6 text-slate-400">
-              Comece <button className=" font-medium text-lime-500 hover:underline ">gravando uma nota </button> em áudio ou se preferir <button onClick={HandleStartEdition} className= "font-medium text-lime-500 hover:underline"> ultilize apenas texto.</button>
+              Comece <button type="button" onClick={handleStartRecording} className= "font-medium text-lime-500 hover:underline">
+                gravando uma nota </button> em áudio ou se preferir <button type="button" onClick={HandleStartEdition} className= "font-medium text-lime-500 hover:underline"> ultilize apenas texto.</button>
                </p>
                ) : ( 
                  /* se não faça  */
-                 <textarea autoFocus className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
-                 onChange={handleContenChange} /*criando um evento para voltar o modal para 
+                 <textarea 
+                 autoFocus className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
+                 
+                 onChange={handleContentChange} 
+                 value={content}
+                 
+                 /*onchance > esta observando toda mudança e salvando dentro do meu content:(useState) na NewNoteCard */ /*criando um evento para voltar o modal para 
                  a tela de seleção caso o usuario apague tudo que escreveu/* 
                  
                  obs:
@@ -114,28 +205,40 @@ toast.success('nota criada com sucesso')
 
        
                 </div> 
-               
-                <button
-               type="submit"
-               className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
-               >
-                    <span className="text-slate-700 font-bold group-hover:underline ">Salvar nota</span> 
-                    { /* group > para aplicar um estilo em um elemento e para que ele seja acionado ao interagir
-                     a partir do elemento pai e nao somente quando chegar no elemento filho, settamos a propriedade group no elemento pai e no filho*/}
-              </button>
-                     </form>
-             
-              </Dialog.Content> {/* .content > determina o que irá aparecer quando eu clicar no modal*/}
-           </Dialog.Portal>
-       </Dialog.Root>
 
+                {isRecording ? (
+              <button
+                type="button"
+                onClick={handleStopRecording}
+                className="w-full flex items-center justify-center gap-2 bg-slate-900 py-4 text-center text-sm text-slate-300 outline-none font-medium hover:text-slate-100"
+              >
+
+
+                <div className="size-3 rounded-full bg-red-500 animate-pulse" />
+                Gravando! (clique p/ interromper)
+              </button>
+            ) : (
+              <button
+                type="button"
+                onClick={HandleSaveNote}
+                className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
+              >
+                Salvar nota
+              </button>
+            )}
+          </form>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 }
 
 
+
+
 /* função cards-Notes   */
 
-export function Notecard({note}: NoteCardProps){
+export function Notecard({note, onNoteDeleted}: NoteCardProps){
 return (
           /* passando para o root / elemento raiz da nossa aplicação - html */
  <Dialog.Root>
@@ -143,8 +246,9 @@ return (
     <Dialog.Trigger /* seleciona o elemento no qual eu quero que apareça o modal */
      className="text-left flex flex-col rounded-md bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
              {/* hover:ring > funciona como uma borda porem sem modificar o tamanho do elemento*/}
+             
               <span className="text-sm font-medium text-slate-300">
-                {note.date.toISOString()}
+              {formatDistanceToNow(note.date, {locale: ptBR, addSuffix: true})} 
               </span>
 
             <p className="text-sm leading-6 text-slate-400">
@@ -159,7 +263,7 @@ return (
            
            <Dialog.Overlay className="inset-0 fixed bg-black/50"/> {/* overlay > adiciona o efeito do modal, de estar por cima da aplicação/ inset-0 > para ocupar a tela toda/ fixed para nao ter scroll*/}
             
-            <Dialog.Content className="fixed left-1/2 top-1/2 overflow-hidden -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none "> 
+            <Dialog.Content className="fixed inset-0 md:inset-auto md:left-1/2 md:top-1/2 overflow-hidden md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none "> 
             {/* -translate >  força o elemento a fixar-se no centro da tela - pois por padrao ele se ajuta ao centro baseando pela ponta superior esquerda do objeto */}
               {/* h-[60vh] vh define medida em relação a tela, quando usamos % ele usa como base o tamanho do elemento pai */}
              
@@ -187,6 +291,7 @@ return (
                
                 <button
                type="button"
+               onClick=  {() => onNoteDeleted(note.id)}
                className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
               >
                      Deseja <span className="text-red-400 group-hover:underline ">apagar essa nota?</span> 
@@ -210,5 +315,4 @@ return (
 
 
 );
-
 }
